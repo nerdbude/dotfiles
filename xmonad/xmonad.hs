dff2193ba9012ee440b1c54f5791f670bd5c768c
0x01_PH <post@nerdbude.com>
2020-04-15 20:49:59 +0200

    fix syntax

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -199,8 +199,8 @@ myKeys =
 
     -- Screenbrightness
     	
-	, ("XF86ManBrightnessUp", spawn "xbacklight -s 100")
-	, ("XF86MonBrightnessDown", spawn "xbacklight -s 80")
+	, ("XF86ManBrightnessUp", spawn "xbacklight -set 100")
+	, ("XF86MonBrightnessDown", spawn "xbacklight -set 80")
         ] where nonNSP          = WSIs (return (\ws -> W.tag ws /= "nsp"))
                 nonEmptyNonNSP  = WSIs (return (\ws -> isJust (W.stack ws) && W.tag ws /= "nsp"))
                 
